0dcece2f5e2165f1bcbd8244a7b47b81deee1772
Carlos Melegrito <cj@mlgrto.com>
2019-03-12 09:23:44 +1100

    Create symbols list

--- src/plugin.js
+++ src/plugin.js
@@ -4,19 +4,30 @@ import BrowserWindow from 'sketch-module-web-view'
 function Dropbear (context) {
 
 	const document = sketch.fromNative(context.document)
+	const selection = sketch.fromNative(context.selection)
 	const symbols = document.getSymbols()
 
 	const options = {
 		identifier: 'com.mlgrto.Dropbear',
+		titleBarStyle: 'hidden',
 		alwaysOnTop: true,
+		devTools: true,
+		height: 320,
+		width: 320,
 	}
 
 	const browserWindow = new BrowserWindow(options)
 
 	browserWindow.loadURL(require('./index.html'))
 
-	browserWindow.webContents.on('requestSymbolCount', function (s) {
-		browserWindow.webContents.executeJavaScript(`updateOutput(${symbols.length})`)
+	for (var i = 0; i < symbols.length; i++) {
+		var name = symbols[i].name
+		var id = symbols[i].id
+		browserWindow.webContents.executeJavaScript(`list("${name},${id}")`)
+	}
+
+	browserWindow.webContents.on('symbolClicked', function (s) {
+		sketch.UI.message(s)
 	})
 }
 
