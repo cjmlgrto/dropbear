848db5d865ab75d5b121b93f65a87ada25080f22
Carlos Melegrito <cj@mlgrto.com>
2019-03-11 20:41:53 +1100

    Enable two-way communication

--- src/plugin.js
+++ src/plugin.js
@@ -1,53 +1,25 @@
 import sketch from 'sketch'
+import BrowserWindow from 'sketch-module-web-view'
 
 function Dropbear () {
-	// Constants
-	const panelWidth = 320
-	const panelHeight = 320
+	const options = {
+		identifier: 'com.mlgrto.Dropbear',
+		alwaysOnTop: true,
+	}
 
-	// Initialise panel
-	var identifier = 'com.mlgrto.Dropbear'
-	var threadDictionary = NSThread.mainThread().threadDictionary()
+	const browserWindow = new BrowserWindow(options)
 
-	if (threadDictionary[identifier]) return
+	browserWindow.loadURL(require('./index.html'))
 
-	var panel = NSPanel.alloc().init()
-	threadDictionary[identifier] = panel
-	COScript.currentCOScript().setShouldKeepAround_(true)
+	browserWindow.webContents
+		.executeJavaScript('display("Hello!")')
+		.then(res => {
+			sketch.UI.message('Connected.')
+		})
 
-	// Setup panel
-	panel.setFrame_display(NSMakeRect(0, 0, panelWidth, panelHeight), true)
-	panel.setStyleMask(NSTexturedBackgroundWindowMask | NSTitledWindowMask | NSClosableWindowMask | NSFullSizeContentViewWindowMask)
-	panel.setBackgroundColor(NSColor.whiteColor())
-
-	panel.title = 'Dropbear'
-	panel.titlebarAppearsTransparent = true
-
-	panel.standardWindowButton(NSWindowMiniaturizeButton).setHidden(true)
-	panel.standardWindowButton(NSWindowZoomButton).setHidden(true)
-
-	var closeButton = panel.standardWindowButton(NSWindowCloseButton)
-	closeButton.setCOSJSTargetFunction(function(sender) {
-		panel.close();
-		threadDictionary.removeObjectForKey(identifier);
-		COScript.currentCOScript().setShouldKeepAround_(false);
-	});
-
-	var vibrancy = NSVisualEffectView.alloc().initWithFrame(NSMakeRect(0, 0, panelWidth, panelHeight))
-	vibrancy.setAppearance(NSAppearance.appearanceNamed(NSAppearanceNameVibrantLight))
-	vibrancy.setBlendingMode(NSVisualEffectBlendingModeBehindWindow)
-	panel.contentView().addSubview(vibrancy)
-
-	panel.center()
-	panel.makeKeyAndOrderFront(null)
-	panel.setLevel(NSFloatingWindowLevel)
-
-	// Setup web view
-	var webView = WebView.alloc().initWithFrame(NSMakeRect(0, 0, panelWidth, panelHeight - 44))
-	var request = NSURLRequest.requestWithURL(context.plugin.urlForResourceNamed("dropbear.html"))
-	webView.mainFrame().loadRequest(request)
-	webView.setDrawsBackground(false)
-	panel.contentView().addSubview(webView)
+	browserWindow.webContents.on('eventEmitted', function(s) {
+		sketch.UI.message(s)
+	})
 }
 
 export default Dropbear
